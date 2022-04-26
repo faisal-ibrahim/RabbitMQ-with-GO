dd232252600dcdf6959ae6aa8618e938be011d3d
Faisal Ibrahim <faisal.im048@gmail.com>
2022-04-26 23:56:20 +0200

    code improvement

--- main.go
+++ main.go
@@ -1,1 +1,14 @@
-package RabbitMQ_with_GO
+package main
+
+import "fmt"
+
+func Run() error {
+	return nil
+}
+
+func main() {
+	if err := Run(); err != nil {
+		fmt.Println("Error Setting Up our application")
+		fmt.Println(err)
+	}
+}

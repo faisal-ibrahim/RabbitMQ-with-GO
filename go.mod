12ab14d913c5bee402191c5988cd2686701152dc
Faisal Ibrahim <faisal.im048@gmail.com>
2022-05-07 02:00:30 +0200

    added rabbitmq package and update amqp

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/faisal-ibrahim/RabbitMQ-with-GO
 
 go 1.17
+
+require github.com/streadway/amqp v1.0.0 // indirect
--- internal/rabbitmq/rabbitmq.go
+++ internal/rabbitmq/rabbitmq.go
@@ -1,1 +1,15 @@
 package rabbitmq
+
+import "github.com/streadway/amqp"
+
+// Service -
+type Service interface {
+	Connect() error
+	Publish(message string) error
+}
+
+// RabbitMQ -
+type RabbitMQ struct {
+	Conn    *amqp.Connection
+	Channel *amqp.Channel
+}

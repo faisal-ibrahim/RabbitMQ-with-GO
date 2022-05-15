acd7536fd2836f403224d13327dd081780ed131c
Faisal Ibrahim <faisal.im048@gmail.com>
2022-05-15 14:41:04 +0200

    publish rabbitmq message

--- internal/rabbitmq/rabbitmq.go
+++ internal/rabbitmq/rabbitmq.go
@@ -1,6 +1,9 @@
 package rabbitmq
 
-import "github.com/streadway/amqp"
+import (
+	"fmt"
+	"github.com/streadway/amqp"
+)
 
 // Service -struct
 type Service interface {
@@ -12,6 +15,28 @@ type Service interface {
 type RabbitMQ struct {
 	Conn    *amqp.Connection
 	Channel *amqp.Channel
+}
+
+// Publish - publishes a message to the queue
+func (r *RabbitMQ) Publish(message string) error {
+	// attempt to publish a message to the queue!
+	err := r.Channel.Publish(
+		"",
+		"TestQueue",
+		false,
+		false,
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(message),
+		},
+	)
+
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Successfully Published Message to Queue")
+	return nil
 }
 
 // NewRabbitMQService - returns a pointer to a new RabbitMQ service
